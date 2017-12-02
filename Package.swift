80025ee1be292cd212be12a8abc70fdbf70df13a
Rene Hexel <rhx@users.noreply.github.com>
2017-12-03 06:24:54 +1000

    Depend on HAP

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "httpswitch-hap",
+    products: [
+        .executable(name: "httpswitch-hap", targets: ["httpswitch-hap"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/bouke/HAP.git", .branch("master")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "httpswitch-hap",
-            dependencies: []),
+        .target(name: "httpswitch-hap", dependencies: ["HAP"]),
     ]
 )
